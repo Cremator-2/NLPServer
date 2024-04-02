797b7f03718d2473ca36fa1d9e2b69ea46468f22
Cremator <ilya2azin@gmail.com>
2024-04-02 20:59:28 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -3,17 +3,15 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from api import healthcheck_router
-from logger import get_logger
+from app.api import healthcheck_router
+from app.logger import get_logger
 
 logger = get_logger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(_):
-    logger.info("Start API")
     yield
-    logger.info("Stop API")
 
 app = FastAPI(lifespan=lifespan)
 
@@ -25,7 +23,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=8001,
+        port=44596,
         log_level="info",
         forwarded_allow_ips="*",
         proxy_headers=True,
