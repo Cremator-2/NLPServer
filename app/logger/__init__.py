46c49be85e66db275fdf033fc9023c43573794c4
Cremator <ilya2azin@gmail.com>
2024-04-03 15:49:01 +0300

    change in logs

--- app/logger/__init__.py
+++ app/logger/__init__.py
@@ -1,20 +1,55 @@
 import logging
 
 
-def get_logger(name: str, log_level: int = logging.INFO, formatter: str = None) -> logging.Logger:
+class CustomFormatter(logging.Formatter):
+
+    c = "\x1b[36;21m"
+    g = "\x1b[32;21m"
+    y = "\x1b[33;21m"
+    r = "\x1b[31;21m"
+    b = "\x1b[34;21m"
+
+    bold = "\033[1m"
+    end = "\x1b[0m"
+
+    level_name = "%(levelname)s"
+    name = "%(name)s:%(lineno)d"
+    message = "%(message)s"
+    asctime = "%(asctime)s"
+
+    ind = ":\t  "
+    hyp = " - "
+    ob = " ["
+    cb = "] "
+
+    level_name = level_name + end
+    message = bold + message + end
+    asctime = ob + c + asctime + end + cb
+
+    constructor = level_name + ind + name + hyp + message + asctime
+
+    FORMATS = {
+        logging.DEBUG: b + constructor,
+        logging.INFO: g + constructor,
+        logging.WARNING: y + constructor,
+        logging.ERROR: r + constructor,
+        logging.CRITICAL: r + bold + constructor,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def get_logger(name: str, log_level: int = logging.INFO) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
     logger.propagate = False
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(log_level)
-
-    if formatter:
-        formatter = logging.Formatter(formatter)
-        console_handler.setFormatter(formatter)
-    else:
-        default_formatter = logging.Formatter('%(levelname)s:\t  %(message)s - %(name)s:%(lineno)d [%(asctime)s]')
-        console_handler.setFormatter(default_formatter)
+    console_handler.setFormatter(CustomFormatter())
 
     logger.addHandler(console_handler)
     return logger
